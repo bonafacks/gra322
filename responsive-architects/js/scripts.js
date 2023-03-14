51fbf387c1615078c8eb42d399b8c7fbad2c47b5
Jose Villalobos <jgvilla1@asu.edu>
2023-03-14 15:26:33 -0700

    everthing

--- responsive-architects/js/scripts.js
+++ responsive-architects/js/scripts.js
@@ -2,5 +2,29 @@ $('.menu-button') .click(function(){
     $('nav').toggleClass('open');
 });
 
-
+//Intialize Animate and scroll
 AOS.init();
+
+//intialize Jarallax
+jarallax(document.querySelectorAll('.jarallax'), {
+    speed: 0.5,
+    type: 'scale'
+  });
+
+
+  // Also can pass in optional settings block
+ var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+//intialize tinyslider
+  var slider = tns({
+    container: '.my-slider',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true
+  });
